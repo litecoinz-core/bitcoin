cb1ab2aba6d89a429d035f2a3fb8ad6442564a48
LitecoinZ <team@litecoinz.info>
2019-11-29 14:52:56 +0100

    [gui] Nuke some circular dependencies (#17513)

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -15,12 +15,13 @@
 
 #include <consensus/consensus.h>
 #include <interfaces/node.h>
+#include <interfaces/wallet.h>
 #include <key_io.h>
-#include <validation.h>
+#include <policy/policy.h>
 #include <script/script.h>
 #include <timedata.h>
 #include <util/system.h>
-#include <policy/policy.h>
+#include <validation.h>
 #include <wallet/ismine.h>
 
 #include <stdint.h>
