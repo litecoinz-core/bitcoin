f55bd1db65ea1036c78956be64c847ce5743bb81
LitecoinZ <team@litecoinz.info>
2019-11-29 23:27:02 +0100

    Fix copyright year value

--- src/init.cpp
+++ src/init.cpp
@@ -555,7 +555,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/litecoinz-core/litecoinz>";
     const std::string URL_WEBSITE = "<https://litecoinz.org>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2009, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2017, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software.").translated,
